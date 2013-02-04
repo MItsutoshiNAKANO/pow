f9c8ff061b1d912a9d85202fd90812ccc2e852bf
Mitsu <bkbin005@rinku.zaq.ne.jp>
2013-02-04 09:09:40 +0900

    %f -> %e .

--- pow/pow.c
+++ pow/pow.c
@@ -1,6 +1,8 @@
 /** */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "str2pow.h"
 
 int
@@ -11,6 +13,11 @@ main(int argc, char *argv[])
     exit(2);
   }
   double d = str2pow(argv[1], argv[2]);
-  printf("%f\n", d);
+  if (printf("%g\n", d) <= 0) {
+    int save_errno = errno;
+    char *p = strerror(save_errno);
+    fprintf(stderr ,"%s: output_error: %s, %d\n", argv[0], p, save_errno);
+    exit(3);
+  }
   exit(0);
 }
