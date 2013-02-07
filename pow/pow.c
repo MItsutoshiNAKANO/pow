7b86049b90fcdb4a5cc1a0936d511bd2c0e70532
Mitsu <bkbin005@rinku.zaq.ne.jp>
2013-02-07 13:28:33 +0900

    Modified for locale .

--- pow/pow.c
+++ pow/pow.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <locale.h>
+#include <nl_types.h>
 #include <getopt.h>
 #include <config.h>
 #include "str2pow.h"
@@ -13,36 +15,44 @@ print_version(FILE *file)
   return fprintf(file, "%s %s\n", PACKAGE, VERSION);
 }
 
+static nl_catd cd;
+
 static int
 print_help (FILE *file)
 {
   /* TRANSLATORS: --help output 1 (synopsis)     no-wrap */
   return
     fprintf(file,
-	    "Usage: pow x y" "\n"
-	    "or: pow [--help|--version]" "\n\n"
-	    "The pow returns the value of x raised to the power of y ." "\n\n\t"
-	    "--help print this help message ." "\n\t"
-	    "--version print version ." "\n\n"
-	    "Examples: pow 2 3" "\n\n"
-	    "Report bugs to: "  PACKAGE_BUGREPORT "\n\n"
-
-	    /*
-	    "Author: " PACKAGE_BUGREPORT "\n\n"
-	    "COPYRIGHT:" "\n\n"
-	    "SEE ALSO:" "\n"
-	    "ENVIRONMENT:" "\n\n"
-	    "FILES:" "\n\n"
-	    "other:" "\n\n"
-	    */
-	    );
+	    catgets(cd, NL_SETD, 1,
+		    "Usage: pow x y" "\n"
+		    "or: pow [--help|--version]" "\n\n"
+		    "The pow returns the value of x raised to the power of y ."
+		    "\n\n\t"
+		    "--help print this help message ." "\n\t"
+		    "--version print version ." "\n\n"
+		    "Examples: pow 2 3" "\n\n"
+		    "Report bugs to: %s\n\n"
+		    "Written by %s\n\n"
+		    "Copyright (C) %s\n"
+		    /*
+		     * "See Also \n\t" "str2pow(3) pow(3)" "\n\n"
+		     * "Environment" "\n\n"
+		     * "Files" "\n\n"
+		     * "other" "\n\n"
+		     */),
+	    PACKAGE_BUGREPORT, PACKAGE_BUGREPORT, PACKAGE_BUGREPORT);
 }
-
-
 
 int
 main(int argc, char *argv[])
 {
+  cd = catopen("pow.cat", 0);
+
+  if (!setlocale(LC_ALL, "")) {
+    fprintf(stderr, "setlocale() error");
+    exit(5);
+  }
+
   static const struct option longopts[] = {
     { "help", no_argument, NULL, 'h' },
     { "version", no_argument, NULL, 'v' },
@@ -66,16 +76,15 @@ main(int argc, char *argv[])
   }
 
   if (lose || argc - optind < 2) {
-    fprintf(stderr ,"usage %s num num\n", argv[0]);
     print_help(stderr);
-    exit(2);
+    exit(3);
   }
   double d = str2pow(argv[optind], argv[optind + 1]);
   if (printf("%g\n", d) <= 0) {
     int save_errno = errno;
     char *p = strerror(save_errno);
     fprintf(stderr ,"%s: output_error: %s, %d\n", argv[0], p, save_errno);
-    exit(3);
+    exit(4);
   }
   exit(EXIT_SUCCESS);
 }
