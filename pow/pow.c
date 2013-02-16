bca38163e1e52656073a2eac7a1e67cea5b233a9
Mitsu <bkbin005@rinku.zaq.ne.jp>
2013-02-16 15:29:14 +0900

    Changed to use intltool .

--- pow/pow.c
+++ pow/pow.c
@@ -4,10 +4,13 @@
 #include <string.h>
 #include <errno.h>
 #include <locale.h>
-#include <nl_types.h>
+// #include <nl_types.h>
 #include <getopt.h>
+#include <libintl.h>
 #include <config.h>
 #include "str2pow.h"
+
+#define _(str) gettext (str)
 
 enum exit_stat {
   EX_NORMAL, EX_SUBNORMAL, EX_ZERO, EX_MINUS_INFINITE, EX_INFINITE, EX_NAN,
@@ -20,7 +23,6 @@ print_version(FILE *file)
   return fprintf(file, "%s %s\n", PACKAGE, VERSION);
 }
 
-static nl_catd cd;
 
 static int
 print_help (FILE *file)
@@ -28,35 +30,34 @@ print_help (FILE *file)
   /* TRANSLATORS: --help output 1 (synopsis)     no-wrap */
   return
     fprintf(file,
-	    catgets(cd, NL_SETD, 1,
-		    "Usage: pow x y" "\n"
-		    "or: pow [--help|--version]" "\n\n"
-		    "The pow returns the value of x raised to the power of y ."
-		    "\n\n\t"
-		    "--help print this help message ." "\n\t"
-		    "--version print version ." "\n\n"
-		    "Examples: pow 2 3" "\n\n"
-		    "Report bugs to: %s\n\n"
-		    "Written by %s\n\n"
-		    "Copyright (C) %s\n"
-		    /*
-		     * "See Also \n\t" "str2pow(3) pow(3)" "\n\n"
-		     * "Environment" "\n\n"
-		     * "Files" "\n\n"
-		     * "other" "\n\n"
-		     */),
+	    _("Usage: pow x y" "\n"
+	      "or: pow [--help|--version]" "\n\n"
+	      "The pow returns the value of x raised to the power of y ."
+	      "\n\n\t"
+	      "--help print this help message ." "\n\t"
+	      "--version print version ." "\n\n"
+	      "Examples: pow 2 3" "\n\n"
+	      "Report bugs to: %s\n\n"
+	      "Written by %s\n\n"
+	      "Copyright (C) %s\n"
+	      /*
+	       * "See Also \n\t" "str2pow(3) pow(3)" "\n\n"
+	       * "Environment" "\n\n"
+	       * "Files" "\n\n"
+	       * "other" "\n\n"
+	       */),
 	    PACKAGE_BUGREPORT, PACKAGE_BUGREPORT, PACKAGE_BUGREPORT);
 }
 
 int
 main(int argc, char *argv[])
 {
-  cd = catopen("pow.cat", 0);
-
   if (!setlocale(LC_ALL, "")) {
-    fprintf(stderr, "setlocale() error");
+    fprintf(stderr, _("setlocale() error"));
     exit(EX_LOCALE);
   }
+  bindtextdomain(PACKAGE, LOCALEDIR);
+  textdomain(PACKAGE);
 
   static const struct option longopts[] = {
     { "help", no_argument, NULL, 'h' },
@@ -103,8 +104,8 @@ main(int argc, char *argv[])
       int save_errno = errno;
       printf("%g\n", d);
       fprintf(stderr,
-	      "Why this code was called?!\n"
-	      "%d:%s\n", save_errno, strerror(save_errno));
+	      _("Why this code was called?!\n"
+		"%d:%s\n"), save_errno, strerror(save_errno));
       abort();
     }
     break;
@@ -120,7 +121,7 @@ main(int argc, char *argv[])
 
   if (printf("%g\n", d) <= 0) {
     int save_errno = errno;
-    fprintf(stderr ,"pow: output_error:%d: %s\n", save_errno,
+    fprintf(stderr ,_("pow: output_error:%d: %s\n"), save_errno,
 	    strerror(save_errno));
     exit(EX_OUTPUT_ERROR);
   }
